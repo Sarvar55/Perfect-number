f5534a9945411e87e67df966c22f7f33877636f2
Sarvar55 <76266468+Sarvar55@users.noreply.github.com>
2021-04-19 12:09:08 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+int perfect(int num){
+    int i=1;
+    int total=0;
+   while(i<num){
+      if(num % i==0)
+         total+=i;
+    i++;
+   }
+   if(total==num)
+      return  1;
+   else
+   return 0;
+
+}
+
+int main()
+{
+    int num;
+    int result;
+    printf("Please enter a number\n");
+    scanf("%d",&num);
+    result=perfect(num);
+    if(result==1)
+        printf("%d  number is the perfect number",num);
+    else
+        printf("%d number is the perfect number not",num);
+    return 0;
+}
